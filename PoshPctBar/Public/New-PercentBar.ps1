d9034bf699ead1a857fad5ed07743c20e72374e8
Joshua King <windosbk+github@gmail.com>
2015-10-05 14:02:27 +1300

    First release almost ready

--- PoshPctBar/Public/New-PercentBar.ps1
+++ PoshPctBar/Public/New-PercentBar.ps1
@@ -10,19 +10,25 @@
 	    .EXAMPLE
     #>
 
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName='Border')]
     param
     (
         [Parameter(Mandatory = $true,
                    Position = 0,
                    ValueFromPipeline = $True,
                    ValueFromPipelineByPropertyName = $True,
-                   HelpMessage = '?')]
+                   HelpMessage = 'What is the percentage?')]
         [ValidateNotNullOrEmpty()]
         [decimal] $Percentage,
 
-        [ValidateSet('{}', '()', '||', '/\', '--')]
-        [string] $Border = '[]'
+        [Parameter(Mandatory=$false,
+                   ParameterSetName='Border')]
+        [ValidateSet('[]', '{}', '()', '||', '/\', '--')]
+        [string] $Border = '[]',
+
+        [Parameter(Mandatory=$true,
+                   ParameterSetName='NoBorder')]
+        [switch] $NoBorder
     )
 
     begin
@@ -30,6 +36,20 @@
 
     process
 	{
-        $Border[0] + 'x' * ([System.Math]::Round(( $Percentage )*100)/10) + '.' * (10-([System.Math]::Round(( $Percentage )*100)/10)) + $Border[1]
+        try
+        {
+            $PercentBar = 'x' * ([System.Math]::Round(( $Percentage )*100)/10) + '.' * (10-([System.Math]::Round(( $Percentage )*100)/10))
+
+            if (!$NoBorder)
+            {
+                $PercentBar = $Border[0] + $PercentBar + $Border[1]
+            }
+
+            $PercentBar
+        }
+        catch
+        {
+            Throw $_.Exception
+        }
     }
 }
