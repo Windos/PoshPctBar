5a19cf17d1179fd0c154411a0fa33203e8f3efdf
Joshua King <windosbk+github@gmail.com>
2015-10-05 16:40:40 +1300

    Had issues on PS2

--- PoshPctBar/PoshPctBar.psm1
+++ PoshPctBar/PoshPctBar.psm1
@@ -1,16 +1,1 @@
-$Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
-$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
-
-Foreach($Import in @($Public + $Private))
-{
-    Try
-    {
-        . $Import.fullname
-    }
-    Catch
-    {
-        Write-Error -Message "Failed to import function $($Import.fullname): $_"
-    }
-}
-
-Export-ModuleMember -Function $Public.Basename
+. $PSScriptRoot\Public\New-PercentBar.ps1
