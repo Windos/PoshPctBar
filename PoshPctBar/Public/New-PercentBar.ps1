839f422f023931984725bf76a3b9c7039a87efbd
Joshua King <windosbk+github@gmail.com>
2015-10-05 13:45:39 +1300

    Initial cmdlet dump

---
+++ PoshPctBar/Public/New-PercentBar.ps1
@@ -0,0 +1,35 @@
+﻿function New-PercentBar
+{
+    <#
+        .SYNOPSIS
+
+	    .DESCRIPTION
+
+	    .EXAMPLE
+
+	    .EXAMPLE
+    #>
+
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory = $true,
+                   Position = 0,
+                   ValueFromPipeline = $True,
+                   ValueFromPipelineByPropertyName = $True,
+                   HelpMessage = '?')]
+        [ValidateNotNullOrEmpty()]
+        [decimal] $Percentage,
+
+        [ValidateSet('{}', '()', '||', '/\', '--')]
+        [string] $Border = '[]'
+    )
+
+    begin
+    {}
+
+    process
+	{
+        $Border[0] + 'x' * ([System.Math]::Round(( $Percentage )*100)/10) + '.' * (10-([System.Math]::Round(( $Percentage )*100)/10)) + $Border[1]
+    }
+}
