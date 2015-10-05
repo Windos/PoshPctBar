6d3956c13b46d3e24e257603b6c213047f063145
Joshua King <windosbk+github@gmail.com>
2015-10-05 15:18:49 +1300

    Comment based help

--- PoshPctBar/Public/New-PercentBar.ps1
+++ PoshPctBar/Public/New-PercentBar.ps1
@@ -2,37 +2,64 @@
 {
     <#
         .SYNOPSIS
+        Creates a new Percentage Bar.
 
 	    .DESCRIPTION
+        The New-PercentBar cmdlet creates a new Percentage Bar, a visual representation of an quanity out of 100.
+
+        The percentage bar is returned as a string with a border. The borderer can be excluded if desired.
 
 	    .EXAMPLE
+        New-PercentBar -Percent 0.5
+
+        This example generates a Percentage Bar that is 50% filled, with a default square bracket border:
+
+        [xxxxx.....]
 
 	    .EXAMPLE
+        New-PercentBar -Percent 0.3 -Border '||'
+
+        This example generates a Percentage Bar that is 30% filled, with a border constructed out of pipe (|) characters:
+
+        |xxx.......|
+
+        .EXAMPLE
+        0.8 | New-PercentBar -NoBorder
+
+        This example takes a decimal value from the pipeline and generates a Percentage Bar that is 80% filled and doesn't have a border:
+
+        xxxxxxxx..
+
+        .INPUTS
+        System.Decimal
+        
+        .LINK
+        https://github.com/Windos/PoshPctBar
     #>
 
     [CmdletBinding(DefaultParameterSetName='Border')]
+    [OutputType("System.String")]
     param
     (
         [Parameter(Mandatory = $true,
                    Position = 0,
                    ValueFromPipeline = $True,
                    ValueFromPipelineByPropertyName = $True,
-                   HelpMessage = 'What is the percentage?')]
+                   HelpMessage = 'Enter the percentage in the form of a decimal value between 0 and 1.')]
         [ValidateNotNullOrEmpty()]
         [decimal] $Percentage,
 
         [Parameter(Mandatory=$false,
+                   Position = 1,
                    ParameterSetName='Border')]
         [ValidateSet('[]', '{}', '()', '||', '/\', '--')]
         [string] $Border = '[]',
 
         [Parameter(Mandatory=$true,
+                   Position = 1,
                    ParameterSetName='NoBorder')]
         [switch] $NoBorder
     )
-
-    begin
-    {}
 
     process
 	{
